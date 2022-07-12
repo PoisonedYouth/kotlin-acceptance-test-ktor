258689b384b52889a2a5d8f6dc166e23a44e840b
Matthias Schenk <matthias.schenk@sevdesk.de>
2022-07-12 09:10:19 +0200

    [V-2] Added database configuration

--- src/main/kotlin/com/poisonedyouth/Application.kt
+++ src/main/kotlin/com/poisonedyouth/Application.kt
@@ -8,5 +8,7 @@ fun main(args: Array<String>): Unit =
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+    DatabaseFactory.init()
+
     configureRouting()
 }
---
+++ src/main/kotlin/com/poisonedyouth/DatabaseFactory.kt
@@ -0,0 +1,31 @@
+package com.poisonedyouth
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object DatabaseFactory {
+
+    fun init() {
+        Database.connect(hikari())
+
+        transaction {
+            SchemaUtils.createMissingTablesAndColumns(AccountTable, AddressTable, CustomerTable)
+        }
+    }
+
+    private fun hikari(): HikariDataSource {
+        val config = HikariConfig()
+        config.driverClassName = "org.h2.Driver"
+        config.jdbcUrl = "jdbc:h2:file:./db"
+        config.username = "root"
+        config.password = "password"
+        config.maximumPoolSize = 3
+        config.isAutoCommit = false
+        config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        config.validate()
+        return HikariDataSource(config)
+    }
+}
